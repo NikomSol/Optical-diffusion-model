dad09c862b1a5bcb879463c452b05cd4b531cf94
NikomSol <nikomsol94@gmail.com>
2021-05-10 20:14:34 +0300

    Create 1d static solver

---
+++ diffusion_equation.py
@@ -0,0 +1,61 @@
+import numpy as np
+from scipy.sparse.linalg import spsolve
+import scipy.sparse as sp
+
+
+class DiffusionEquation():
+
+    @classmethod
+    def static_1D(self, cnf, properties, bound_conditions, sources):
+        N = cnf['N']
+        dn = cnf['dn']
+
+        matrix_equations = sp.dia_matrix(([np.ones(N), -2 * np.ones(N), np.ones(N)], [0, 1, 2]), shape=(N - 2, N))
+
+        matrix_start = sp.coo_matrix(([dn * bound_conditions['Nstart'][0] - bound_conditions['Nstart'][1],
+                                       bound_conditions['Nstart'][1]],
+                                      ([0, 0], [0, 1])), shape=(1, N))
+        matrix_end = sp.coo_matrix(([-bound_conditions['Nend'][1],
+                                     dn * bound_conditions['Nend'][0] + bound_conditions['Nend'][1]],
+                                    ([0, 0], [N - 2, N - 1])), shape=(1, N))
+        matrix = sp.vstack([matrix_start, matrix_equations, matrix_end])
+
+        vector = -dn * dn * sources / properties
+        vector[0] = dn * bound_conditions['Nstart'][2]
+        vector[-1] = dn * bound_conditions['Nend'][2]
+
+        solution = spsolve(matrix.tocsr(), vector)
+        return solution
+
+    @classmethod
+    def static_2D(self, cnf, properties, bound_conditions, sources):
+        N = cnf['N']
+        dn = cnf['dn']
+        M = cnf['M']
+        dm = cnf['dm']
+
+        matrix_equation = sp.coo_matrix(([dm * dm, dm * dm, dn * dn, dn * dn, -2 * dn * dn + -2 * dm * dm],
+                                         ([0, 0, 0, 0, 0],
+                                          [M * (n - 1) + m, M * (n + 1) + m, M * n + m - 1, M * n + m + 1, M * n + m])),
+                                        shape=(1, N*M))
+
+        # solution = spsolve(matrix.tocsr(), vector)
+        return solution
+
+    def time_dependence_1D(self):
+        pass
+
+    def static_1D_axial(self):
+        pass
+
+    def time_dependence_1D_axial(self):
+        pass
+
+    def time_dependence_2D(self):
+        pass
+
+    def static_2D_axial(self):
+        pass
+
+    def time_dependence_2D_axial(self):
+        pass
--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+import numpy as np
+from diffusion_equation import DiffusionEquation
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cnf = {
+    'K': 3,
+    'N': 10,
+    'M': 3,
+    'dk': 0.1,
+    'dn': 0.1,
+    'dm': 0.1
+}
+properties = 0.6 / 4200 / 1000 * np.ones((cnf['K'], cnf['N'], cnf['M']))
+sources = 0 * np.ones((cnf['K'], cnf['N'], cnf['M']))
+start_conditions = np.ones((cnf['N'], cnf['M']))
+bound_conditions = cnf['K'] * [
+    {'Nstart': cnf['M'] * [[1, 0, 1]],
+     'Nend': cnf['M'] * [[1, 0, 1]],
+     'Mstart': cnf['N'] * [[1, 0, 1]],
+     'Mend': cnf['N'] * [[1, 0, 1]]}
+]
 
+properties_static_1D = 0.6 / 4200 / 1000 * np.ones(cnf['N'])
+sources_static_1D = 0 * (10 ** (-6)) * np.ones(cnf['N'])
+bound_conditions_static_1D = {'Nstart': [1, -1, 0], 'Nend': [1, 0, 1]}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+solution_static_1D = DiffusionEquation.static_1D(cnf, properties_static_1D, bound_conditions_static_1D,
+                                                 sources_static_1D)
+print(solution_static_1D)
 
+properties_static_2D = 0.6 / 4200 / 1000 * np.ones(cnf['N'], cnf['M'])
+sources_static_2D = 0 * (10 ** (-6)) * np.ones(cnf['N'], cnf['M'])
+bound_conditions_static_2D = {
+    'Nstart': cnf['M'] * [[1, 0, 1]],
+    'Nend': cnf['M'] * [[1, 0, 1]],
+    'Mstart': cnf['N'] * [[1, 0, 1]],
+    'Mend': cnf['N'] * [[1, 0, 1]]}
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+solution_static_2D = DiffusionEquation.static_2D(cnf, properties_static_2D, bound_conditions_static_2D,
+                                                 sources_static_2D)
+print(solution_static_2D)
