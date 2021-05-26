f29dd0ca8fed920a802b4b40b8065a7b6a5a89ab
nikom <nikomsol94@gmail.com>
2021-05-26 18:42:44 +0300

    electodiffusion 1D V1 work true

--- electrodiffusion_equation.py
+++ electrodiffusion_equation.py
@@ -9,16 +9,18 @@ class ElectroDiffusionEquation():
     def static_1D_real(self, cnf, properties, bound_conditions):
         N = cnf['N']
         dn = cnf['dn']
+        inv_dn = 1 / dn
         eps0 = cnf['eps0']
 
         zeros_N = np.zeros(N)
-        ones_2N = np.ones(2*N)
+        ones_2N = np.ones(2 * N)
 
         sigma = properties['sigma']
         epsilon = properties['epsilon']
         diffusion = properties['diffusion']
 
-        inv_eps = (1 / eps0) * (1 / epsilon)
+        sigma_diffusion = sigma / diffusion
+        inv_eps_dn2 = (1 / eps0) * (1 / epsilon) * dn * dn
 
         potential_start = bound_conditions['N_start_potential']
         potential_end = bound_conditions['N_end_potential']
@@ -26,34 +28,34 @@ class ElectroDiffusionEquation():
         charge_end = bound_conditions['N_end_charge']
 
         matrix_potential_equations = sp.dia_matrix((
-            [ones_2N, -2 * ones_2N, ones_2N, np.concatenate((zeros_N, inv_eps)) * dn * dn],
+            [ones_2N, -2 * ones_2N, ones_2N, np.concatenate((zeros_N, inv_eps_dn2))],
             [0, 1, 2, N + 1]),
             shape=(N - 2, 2 * N))
         matrix_charge_equations = sp.dia_matrix((
-            [np.concatenate((zeros_N, diffusion)),
-             -2 * np.concatenate((zeros_N, diffusion)) + np.concatenate((zeros_N, sigma)) * dn * dn,
-             np.concatenate((zeros_N, diffusion))],
+            [ones_2N,
+             -2 * ones_2N - np.concatenate((zeros_N, sigma_diffusion * inv_eps_dn2)),
+             ones_2N],
             [N, N + 1, N + 2]),
             shape=(N - 2, 2 * N))
 
         matrix_potential_start = sp.coo_matrix((
-            [dn * potential_start[0] - potential_start[1], potential_start[1]],
+            [potential_start[0] - potential_start[1] * inv_dn, potential_start[1] * inv_dn],
             ([0, 0], [0, 1])), shape=(1, 2 * N))
         matrix_potential_end = sp.coo_matrix((
-            [-potential_end[1], dn * potential_end[0] + potential_end[1]],
+            [-potential_end[1] * inv_dn, potential_end[0] + potential_end[1] * inv_dn],
             ([0, 0], [N - 2, N - 1])), shape=(1, 2 * N))
 
         matrix_charge_start = sp.coo_matrix((
-            [-sigma[0] * charge_start[1],
-             sigma[0] * charge_start[1],
-             -diffusion[0] * charge_start[1] + dn * charge_start[0],
-             diffusion[0] * charge_start[1]],
+            [-sigma_diffusion[0] * charge_start[1],
+             sigma_diffusion[0] * charge_start[1],
+             -charge_start[1] + dn * charge_start[0] / diffusion[0],
+             charge_start[1]],
             ([0, 0, 0, 0], [0, 1, N, N + 1])), shape=(1, 2 * N))
         matrix_charge_end = sp.coo_matrix((
-            [-sigma[-2] * charge_end[1],
-             sigma[-1] * charge_end[1],
-             -diffusion[-2] * charge_end[1] + dn * charge_end[0],
-             diffusion[-1] * charge_end[1]],
+            [-sigma_diffusion[-1] * charge_end[1],
+             sigma_diffusion[-1] * charge_end[1],
+             -charge_end[1] + dn * charge_end[0] / diffusion[-1],
+             charge_end[1]],
             ([0, 0, 0, 0], [N - 2, N - 1, 2 * N - 2, 2 * N - 1])), shape=(1, 2 * N))
 
         matrix = sp.vstack([matrix_potential_start, matrix_potential_end,
@@ -61,10 +63,11 @@ class ElectroDiffusionEquation():
                             matrix_potential_equations, matrix_charge_equations])
 
         vector = np.zeros(2 * N)
-        vector[0] = dn * potential_start[2]
-        vector[1] = dn * potential_end[2]
-        vector[2] = dn * charge_start[2]
-        vector[3] = dn * charge_end[2]
+        vector[0] = potential_start[2]
+        vector[1] = potential_end[2]
+        vector[2] = charge_start[2] * dn / diffusion[0]
+        vector[3] = charge_end[2] * dn / diffusion[-1]
 
+        print(matrix.toarray())
         solution = spsolve(matrix.tocsr(), vector)
         return solution
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 
 cnf = {
     'K': 3,
-    'N': 10**5,
+    'N': 4*10 ** 0,
     'M': 3,
     'dk': 0.1,
-    'dn': 10**(-11),
+    'dn': 10 ** (-10),
     'dm': 0.1,
 
     'eps0': 8.85 * 10 ** (-12)
@@ -23,17 +23,17 @@ properties = {
 # start_conditions = np.ones((cnf['N'], cnf['M']))
 
 bound_conditions = {
-    'N_start_potential': [1, 0, -1],
+    'N_start_potential': [1, 0, 0],
     'N_end_potential': [1, 0, 1],
-    'N_start_charge': [0, 1, 0],
+    'N_start_charge': [1, 0, 0],
     'N_end_charge': [0, 1, 0]
 }
 
 solution_static_1D = ElectroDiffusionEquation.static_1D_real(cnf, properties, bound_conditions)
-u,v = solution_static_1D[:cnf['N']],solution_static_1D[cnf['N']:]
+u, v = solution_static_1D[:cnf['N']], solution_static_1D[cnf['N']:]
 
-print(u)
-print(v)
+# print(v[0])
+# print(v[-1])
 
 plt.figure(figsize=(12, 7))
 
@@ -41,6 +41,4 @@ plt.subplot(2, 1, 1)
 plt.plot(u)
 plt.subplot(2, 1, 2)
 plt.plot(v)
-
 plt.show()
-
